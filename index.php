415ec3c8105ae820a6926996f421312fcc716fd1
Barturiot <barturiot500@hotmail.nl>
2016-04-13 11:04:48 +0200

    Hierdoor ga je meteen naar login pagina

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+	header("location:project/view/docent_home.php");
+
+?>
---
+++ project/view/docent_home.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Docent Login</title>
+<link rel="stylesheet" href="../includes/stylesheet.css">
+
+</head>
+<body>
+<center>
+<div id="container"> 
+<div id="lettertype-kopje"><h2>Login</h2></div>
+<div id="lettertype-rest">
+
+ <form action="index_inlog.html" method="post">
+  Username:<br>
+  <input type="text" name="username" value="" required><br>
+  Password:<br>
+  <input type="password" name="password" value="" required><br><br>
+  <input type="submit" value="Login">
+
+</form> 
+
+<br>
+Aanmelden met je Windesheim account:<br>
+Studenten: s1234567@student.windesheim.nl<br>
+Medewerkers: p1234567@windesheim.nl<br>
+<br>
+Wachtwoord vergeten? Ga naar https://password.windesheim.nl
+
+</div>
+</div>
+</center>
+</body>
+</html> 
