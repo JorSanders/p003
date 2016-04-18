4353b45c48058749aa009e78d05363ca08084a31
timvanhooff <mitdestrijder@hotmail.com>
2016-04-18 11:41:06 +0200

    Ik heb DELETE in de user controller gezet

--- project/controller/usercontroller.php
+++ project/controller/usercontroller.php
@@ -1,20 +1,22 @@
 <?php session_start();
 
 	include_once("../classes/db/querymanager.php");
-   	include_once("../classes/model/userstudent.php");
-   	include_once("../classes/model/userdocent.php");
+   	include_once("../classes/model/userclass.php");
 
 	$q = new Querymanager();
 
-
-
-
-
-
-
-
-
-
+	//delete user 
+	if (isset($_GET['id'])&&($_GET['action']=='JA')) {
+		$id = $_GET['id'];
+		$q->deleteUser($id); 
+		header('Location: ../index.php'); 	
+	}
+	
+	//delete user fail
+	if (isset($_GET['id'])&&($_GET['action']=='NEE')) {
+		header('Location: ../index.php'); 
+       	
+	}
 
 
 ?>
