52cdf839ab8f1a594086e03e4fd10babac2be85e
timvanhooff <mitdestrijder@hotmail.com>
2016-04-18 11:29:18 +0200

    Delete user query toegevoegd in de querymanger.php

--- project/classes/db/querymanager.php
+++ project/classes/db/querymanager.php
@@ -12,4 +12,16 @@ class QueryManager {
       // OOP: instantieer een MySQLConnection-object en geef deze als resultaat 
       $this->dbconn = new MySQLConnection();     
     }
+	
+	//Hieronder staan alle queries voor de users:
+		
+    //delete user
+    public function deleteUser($id) {
+		//gedaan
+		$this->dbconn->query("
+			DELETE FROM user
+			WHERE id=$id;");
+    }
+	
+	//Hieronder staan alle queries voor de berichten:
     ?>
