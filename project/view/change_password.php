82fc3a73ce5aef3c56a37e23ebc0671c5e036b95
Barturiot <barturiot500@hotmail.nl>
2016-04-25 13:17:03 +0200

    view change password
    wachtwoord aanpassen

---
+++ project/view/change_password.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+	<title>wachtwoord aanpassen</title>
+</head>
+
+	<body>
+		<?php
+			$studentnummer = $_GET['studentnummer'];
+		?>
+		<form action="../controller/usercontroller.php" method="post">
+			<input type="hidden" name="id" value=" <?php echo $studentnummer; ?> "/>
+			<input type="password" name="old_password" placeholder="Oude wachtwoord"/>
+			<input type="password" name="new_password1" placeholder="Nieuw wachtwoord"/>
+			<input type="password" name="new_password2" placeholder="Herhaal nieuw wachtwoord"/>
+			<button class="submit" type="submit" name="action" value="change_password">Wachtwoord wijzigen</button>
+
+		</form>
+
+
+	</body>
+
+
+
+
+
+
+
+
+</html>
