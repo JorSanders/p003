fa3ebe71ebf3d56ba1d558adda2efd00e79899b0
Barturiot <barturiot500@hotmail.nl>
2016-04-25 13:20:31 +0200

    controller wachtwoord aanpassen toegevoegd

--- project/controller/usercontroller.php
+++ project/controller/usercontroller.php
@@ -1,13 +1,13 @@
 <?php session_start();
 
 	include_once("../classes/db/querymanager.php");
-   	include_once("../classes/model/userclass.php");
+	include_once("../classes/model/userclass.php");
+   	include_once("../classes/model/userstudent.php");
+   	include_once("../classes/model/userdocent.php");
 
 	$q = new Querymanager();
 
-	echo"test";
-	
-	//delete user 
+ 	//delete user 
 	if (isset($_GET['id'])&&($_GET['action']=='JA')) {
 		$id = $_GET['id'];
 		$q->deleteUser($id); 
@@ -19,6 +19,34 @@
 		header('Location: ../index.php'); 
        	
 	}
+
+	//Change user password
+	/*Wachtwoord van de user veranderen, new_password1 en 2 moeten met elkaar vergeleken worden en er moet gecontroleerd worden 
+	of het wachtwoord nog beschikbaar is */
+ 	if (isset($_POST['studentnummer']) && isset($_POST['old_password']) && isset($_POST['new_password1'])&& isset($_POST['new_password2'])
+		&&($_POST['action']=='change_password')) {
+ 		$studentnummer = 1;
+ 		$old_password =	$_POST['old_password'];
+ 		$check = $q->check_password($studentnummer,$old_password);
+ 		$_SESSION['check'] = serialize($check); 
+ 	}
+ 		elseif ($row == 1){
+
+		if ($_POST['new_password1'] == $_POST['new_password2']){
+ 		$studentnummer = 1;
+    	$new_password = $_POST['new_password1'];
+		$user = $q->change_password($studentnummer,$new_password);
+		$_SESSION['user'] = serialize($user);    
+		header('Location: ../index.php');   
+    	}
+    	else {
+    		header('Location: ../change_password'); 
+    	}
+    }
+    	
+
+
+
 
 
 ?>
