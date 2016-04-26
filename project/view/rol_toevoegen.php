0474778818657c9cfbf7308214e9944147a43b09
Barturiot <barturiot500@hotmail.nl>
2016-04-26 10:13:44 +0200

    view van rol toevoegen

---
+++ project/view/rol_toevoegen.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+	<title>Rol toevoegen</title>
+</head>
+
+	<body>
+		
+		<form action="../controller/roleController.php" method="POST">
+			<input type="text" name="rol" placeholder="Rol toevoegen"/>
+			<button class="submit" type="submit" name="action" value="addRol">Rol toevoegen</button>
+
+		</form>
+
+
+	</body>
+
+
+
+
+
+
+
+
+</html>
