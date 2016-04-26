dd16cd84787e358cebfcc13b5c2c34921f5d2d78
Barturiot <barturiot500@hotmail.nl>
2016-04-26 13:04:44 +0200

    Get i.p.v post

--- project/view/rol_toevoegen.php
+++ project/view/rol_toevoegen.php
@@ -6,7 +6,7 @@
 
 	<body>
 		
-		<form action="../controller/roleController.php" method="POST">
+		<form action="../controller/roleController.php" method="GET">
 			<input type="text" name="rol" placeholder="Rol toevoegen"/>
 			<button class="submit" type="submit" name="action" value="addRol">Rol toevoegen</button>
 
