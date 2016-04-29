e23dc084154d399a30f1b9e9e10af4937c3082c8
Barturiot <barturiot500@hotmail.nl>
2016-04-29 11:33:43 +0200

    link toegevoegd

---
+++ project/view/index.php
@@ -0,0 +1,4 @@
+
+<a href="http://team157.p004.nl/project/controller/RoleController.php?action=findAllRole">Gebruiker toevoegen</a>
+<a href="overview_subjects.php">Overzicht lessen</a>
+<a href="AddRole.php">rol toevoegen</a>
