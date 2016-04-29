5f33719fa62f8a5e3a61d15509bcde507c57e7a5
Barturiot <barturiot500@hotmail.nl>
2016-04-29 11:32:58 +0200

    AddRole naam wijziging

---
+++ project/view/AddRole.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+	<title>Rol toevoegen</title>
+</head>
+
+	<body>
+		<table>
+			<tr><th><h3>Rol toevoegen:</h3></th></tr>
+		<form action="../controller/RoleController.php" method="POST">
+			<tr><td>Rol:</td></tr>
+			<tr><td><input name="action" type="hidden" value="insertRole"></td></tr>
+			<tr><td><input type="text" name="role" placeholder="Rol toevoegen"/></td></tr>
+			<tr><td><input name="submit" type="submit" value="Rol toevoegen"></td></tr>
+		</form>
+	</table>
+
+
+	</body>
+
+
+
+
+
+
+
+
+</html>
