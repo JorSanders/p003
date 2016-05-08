ae8dda3b71aa11fd2af7987c85d66b10ae582319
JorSanders <jorsanders@hotmail.com>
2016-05-08 16:53:58 +0200

    student kan zich aanwezig melden. en omg IK HAAT PDO MODEL

--- project/classes/db/querymanager.php
+++ project/classes/db/querymanager.php
@@ -14,7 +14,6 @@ class QueryManager {
 		$this->pdomodel = new PDOModel();
 		$this->pdomodel->connect("localhost", "team157_user", "0URTAeesrX", "team157_db");	  
     }
-	
 		
     //delete user
 	// TODO make PDO
@@ -36,10 +35,13 @@ class QueryManager {
     }  
 	
 	// Get all subjects from one teacher
-	public function getSubjectsFromDocent($owner_id) {   
+	public function getSubjectsFromDocent($owner_id) {
+		$this->__construct();	
+	
 		$this->pdomodel->where("owner_id",$owner_id,"=");
 		$this->pdomodel->orderByCols = array("subject_id DESC");
 		$result =  $this->pdomodel->select("subject");
+		
 		
 		if(!empty($result[0])){
 			foreach($result as $dbItem){
@@ -52,6 +54,9 @@ class QueryManager {
 	
 	// Add a subject
 	public function addSubject($subject_name, $owner_id){
+		$this->__construct();	
+
+		
 		$insertSubject["subject_id"] = "NULL"; 
 		$insertSubject["subject_name"] = "$subject_name";
 		$insertSubject["owner_id"] = "$owner_id";
@@ -61,7 +66,9 @@ class QueryManager {
 	}
 
 	// Get all lessons from a subject
-	public function getLessonsFromSubject($subject_id) {         
+	public function getLessonsFromSubject($subject_id) { 
+		$this->__construct();	
+	
 		$this->pdomodel->where("subject_id",$subject_id,"=");
 		$this->pdomodel->orderByCols = array("lesson_id DESC");
 		$result =  $this->pdomodel->select("lesson");
@@ -77,6 +84,8 @@ class QueryManager {
 	
 	// Add a lesson
 	public function addLesson($lesson_name, $subject_id){
+		$this->__construct();	
+
 		$insertLesson["lesson_id"] = "NULL"; 
 		$insertLesson["lesson_name"] = "$lesson_name";
 		$insertLesson["code"] = 0;
@@ -87,6 +96,8 @@ class QueryManager {
 	
 	// update lesson code
 	public function updateLessonCode($lesson_id, $code){
+		$this->__construct();	
+
 		$updateLesson["code"] = "$code";
 		$this->pdomodel->where("lesson_id", $lesson_id);
 		$this->pdomodel->update("lesson", $updateLesson);
@@ -94,6 +105,8 @@ class QueryManager {
 	
 	// get all codes
 	public function getAllCodes(){
+		$this->__construct();	
+
 		$this->pdomodel->columns = array("code");
 		$result =  $this->pdomodel->select("lesson");
 		
@@ -120,5 +133,45 @@ class QueryManager {
         return $roleList;
     }
 	
+	// add a user to a class
+	public function addUser_Lesson($lesson_id, $user_id){
+		$this->__construct();	
+
+		$insertUser_lesson["lesson_id"] = $lesson_id; 
+		$insertUser_lesson["user_id"] = $user_id;
+		$insertUser_lesson["active"] = "true";
+		$this->pdomodel->insert("user_lesson", $insertUser_lesson);
+	}
+	
+	// gets the lesson id bases on the random code
+	public function getLessonIdByCode($code){
+		$this->__construct();	
+
+		$this->pdomodel->where("code", $code,"=");
+		$this->pdomodel->columns = array("lesson_id");
+		$result =  $this->pdomodel->select("lesson");	
+				
+		//gets the lesson id from the result array
+		foreach($result as $dbItem){
+			$lesson_id = $dbItem['lesson_id'];
+		}
+			
+		return $lesson_id;
+	}
+	
+	public function getLessonNameById($lesson_id){		
+		$this->__construct();	
+		
+		$this->pdomodel->where("lesson_id", $lesson_id,"=");
+		$this->pdomodel->columns = array("lesson_name");
+		$result =  $this->pdomodel->select("lesson");			
+
+		//gets the lesson id from the result array
+		foreach($result as $dbItem){
+			$lesson_name = $dbItem['lesson_name'];
+		}
+						
+		return $lesson_name;
+	}
 }
 ?>
--- project/controller/subjectcontroller.php
+++ project/controller/subjectcontroller.php
@@ -6,6 +6,7 @@ include_once("../classes/model/subjectClass.php");
 
 $q = new Querymanager();
 
+	//add a subject in the database
 if ($_POST['action']=="addSubject" &&
 	isset($_POST['subject_name']) &&
 	isset($_POST['owner_id'])){
@@ -14,6 +15,7 @@ if ($_POST['action']=="addSubject" &&
 	header("location: ../view/overview_subjects.php");
 }
 	
+	//add a lesson in the database
 if ($_POST['action']=="addLesson" &&
 	isset($_POST['lesson_name']) &&
 	isset($_POST['subject_id'])){
@@ -22,6 +24,7 @@ if ($_POST['action']=="addLesson" &&
 	header("location: ../view/overview_lessons.php?subject_id={$_POST['subject_id']}");
 }	
 
+	//generate a random code for lessons.
 if ($_POST['action']=="generate_code" && 
 	isset($_POST['lesson_id']) &&
 	isset($_POST['subject_id'])){
@@ -43,5 +46,36 @@ if ($_POST['action']=="generate_code" &&
 	header("location: ../view/overview_lessons.php?subject_id={$_POST['subject_id']}");			
 	
 }
+	// add a user to a lesson
+if ($_POST['action']=="add_user_lesson" && 
+	isset($_POST['code']) &&
+	isset($_POST['user_id'])){
+	
+	//checks if code is valid
+	$codeList = $q->getAllCodes();
+	
+	$validCode = false;
+	foreach($codeList as $existingCode){
+		if($_POST['code'] == $existingCode){
+			$validCode = true;
+		}
+	}
+	
+	$_SESSION['validCode'] = $validCode;
+	//if code is valid write to database. afterwards send the user back to the view
+	if($validCode){
+		$lesson_id = $q->getLessonIdByCode($_POST['code']);
+		
+		$q->addUser_Lesson($lesson_id, $_POST['user_id']);
+		
+		//$q->getLessonIdByCode2(12608);
+		$lesson_name = $q->getLessonNameById($lesson_id);
+		$_SESSION['lesson_name'] = $lesson_name;
 
+		header("location: ../view/input_code.php");		
+	}else{
+		$_SESSION['code'] = $_POST['code'];
+		header("location: ../view/input_code.php");	
+	}
+}
 ?>
