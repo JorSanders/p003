f41e63f5247335b8c0504390e1f53b12d646766d
JorSanders <jorsanders@hotmail.com>
2016-05-11 23:47:36 +0200

    index aangepast

--- project/view/index.php
+++ project/view/index.php
@@ -1,5 +1,5 @@
+<?php
 
-<a href="http://team157.p004.nl/project/controller/RoleController.php?action=findAllRole">Gebruiker toevoegen</a>
-<a href="overview_subjects.php">Overzicht lessen</a>
-<a href="AddRole.php">rol toevoegen</a>
-<a href="http://team157.p004.nl/project/controller/usercontroller.php?action=UserList">Lijst</a>
+header("Location: menu.php");
+
+?>
