4e3197c935b348eacf804aaf70b4451659f6392e
timvanhooff <mitdestrijder@hotmail.com>
2016-05-11 14:55:18 +0200

    bootstrap door je moeder

---
+++ project/includes/bootstrap-3.3.6/docs/examples/test/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   <?php include_once("../sticky-footer-navbar/head_bootstrap.html"); ?> 
+  </head>
+
+  <body>
+	<?php include_once("../sticky-footer-navbar/navbar_bootstrap.html"); ?> 
+    
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>dikke lul</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
+      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+    </div>
+
+    <footer>
+	<?php include_once("../sticky-footer-navbar/footer_bootstrap.html"); ?> 
+    </footer>
+
+
+  </body>
+</html>
+
---
+++ project/view/dikke_lul.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   <?php include_once("../project/include/head_bootstrap.html"); ?> 
+  </head>
+
+  <body>
+	<?php include_once("../project/include/navbar_bootstrap.html"); ?> 
+    
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>dikke lul</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
+      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+    </div>
+
+    <footer>
+	<?php include_once("../project/include/footer_bootstrap.html"); ?> 
+    </footer>
+	<?php include_once("../project/include/test_bootstrap.html"); ?> 
+
+  </body>
+</html>
+
